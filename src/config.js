435f905ac9b8ccf1fe4d2fa482c9b1e1bf440f86
dina2050 <dinozavrik.dina95@gmail.com>
2021-02-26 12:06:09 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: " notes-serverlessapp",
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://4bwnxa9ad9.execute-api.us-east-1.amazonaws.com/prod",
+    },
+    cognito: {
+        REGION: "eu-west-3",
+        USER_POOL_ID: "eu-west-3_2RVZszMae",
+        APP_CLIENT_ID: "514qec6vl5l8ngdjtfn7389hm2",
+        IDENTITY_POOL_ID: "eu-west-3:3a655122-637f-4cce-bc3f-f40801834ee1",
+    },
+};
+
+export default config;
